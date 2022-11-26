caca5aac8b1da42da86cbc274254b7799c50310a
DOGUKAN ATALAY <dogukanatalay46@gmail.com>
2022-11-26 20:57:22 +0300

    db connection helper and loader added

---
+++ loaders/db-connection.loader.js
@@ -0,0 +1,14 @@
+const sequelize = require('../scripts/helpers/sequelize-helper.js')
+
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('DB Connected Successfully');
+    })
+    .catch((error) => {
+        console.error(`Unable to connect to the database: ${error}`);
+    });
+
+module.exports = {
+    sequelize,
+};
