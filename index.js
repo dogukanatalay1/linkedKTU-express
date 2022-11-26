da82d55b9e163ff9a7d1b2ebd24793fa0889c53e
DOGUKAN ATALAY <dogukanatalay46@gmail.com>
2022-11-26 20:10:28 +0300

    project init

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+
+const app = express()
+
+app.use(cors())
+
+const port = process.env.PORT
+
+app.get('/', (req, res) => {
+  res.send('welcome')
+})
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`)
+})
