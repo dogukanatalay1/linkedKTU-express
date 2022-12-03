5ca85965df80883dad0c46cc13dc39d0a7a70d22
DOGUKAN ATALAY <dogukanatalay46@gmail.com>
2022-12-03 21:00:02 +0300

    vercel deleted, auth middleware revised

--- routes/student.routes.js
+++ routes/student.routes.js
@@ -7,16 +7,16 @@ const bodyValidator = require('../middlewares/body-validator.middleware');
 const schema = require('../validations/student.validation');
 
 router
-  .route('/login')
-  .post(studentController.login);
-  // .post(bodyValidator(schema.loginValidation), studentController.login);
-
-router
    .route('/')
    .get(studentController.getAllStudents)
 
 router
-  .route('/:id')
-  .get(studentController.getStudent)
+   .route('/:id')
+   .get(studentController.getStudent)
+
+router
+  .route('/login')
+  .post(studentController.login);
+  // .post(bodyValidator(schema.loginValidation), studentController.login);
 
 module.exports = router;
