7fda8f869509ec18362e588699915afc7b011a83
DOGUKAN ATALAY <dogukanatalay46@gmail.com>
2022-12-15 14:48:32 +0300

    post controllers updated

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -4,15 +4,68 @@ const {
   getOneByQuery, updateByQuery, getAll, getOneById, getAllByQuery, create,
 } = require('../services/base-service');
 const JobPost = require('../models/job-post.model');
+const httpStatus = require('http-status');
 
-createPost = async (req, res, next) => {
+getJobPosts = async (req, res, next) => {
+  try {
+    const jobPosts = await getAll(JobPost.name)
+
+    if (jobPosts[0].length === 0) {
+      return next(
+        new ApiError('There are no job posts found!', httpStatus.NOT_FOUND)
+      )
+    }
+
+    ApiDataSuccess.send('Job posts fetched succesfully!', httpStatus.OK, res, jobPosts)
+  } catch (error) {
+    return next(
+      new ApiError(error.message, httpStatus.NOT_FOUND)
+    )
+  }
+};
+
+getJobPostById = async (req, res, next) => {
+  const { id } = req.params;
+
+  try {
+    const jobPost = await getOneById(JobPost.name, id);
+
+    // CHECK HERE!
+    if (jobPost[0].length === 0) {
+      return next(
+        new ApiError(`There are no job post with id of ${id}`, httpStatus.BAD_REQUEST)
+      )
+    }
+
+    ApiDataSuccess.send(`Jobpost with id of ${id} fetched!`, httpStatus.OK, res, jobPost);
+  } catch (error) {
+    return next(
+      new ApiError(error.message, httpStatus.NOT_FOUND)
+    )
+  }
+};
+
+createJobPost = async (req, res, next) => {
   const {
     title, description, company, role, technologies,
     isRemote, salary, isAccepted, applicants,
   } = req.body;
 
   try {
-    const jobPost = create(JobPost.name);
+
+    const jobPostData = {
+      Title: title,
+      Description: description,
+      Company: company,
+      Role: role,
+      Technologies: technologies,
+      IsRemote: isRemote,
+      Salary: salary,
+      isAccepted: isAccepted,
+      Applicants: applicants
+    }
+
+    const jobPost = await create(JobPost.name, jobPostData);
 
     if (!jobPost) {
       return next(
@@ -28,4 +81,25 @@ createPost = async (req, res, next) => {
   }
 };
 
-module.exports = { createPost };
+deleteJobPost = async (req, res, next) => {
+  const { id } = req.params;
+
+  try {
+    const deletedJobPost = await deleteById(JobPost.name, id)
+
+    // CHECK HERE!
+    if (!deletedJobPost) {
+      return next(
+        new ApiError(`Couldn't delete post ${id}`, httpStatus.BAD_REQUEST)
+      )
+    }
+
+    ApiDataSuccess.send(`Job post id: ${id} deleted!`, httpStatus.OK, res, deletedJobPost);
+  } catch (error) {
+    return next(
+      new ApiError(error.message, httpStatus.NOT_FOUND)
+    )
+  }
+};
+
+module.exports = { getJobPosts, getJobPostById, createJobPost, deleteJobPost };
--- routes/post.routes.js
+++ routes/post.routes.js
@@ -4,7 +4,19 @@ const postController = require('../controllers/post.controller');
 const router = express.Router();
 
 router
-  .route('/create')
-  .post(postController.createPost);
+  .route('/jobposts/')
+  .get(postController.getJobPosts)
+
+router
+  .route('/jobposts/:id')
+  .get(postController.getJobPostById)
+
+router
+  .route('/jobposts/create')
+  .post(postController.createJobPost);
+
+router
+  .route('/jobposts/delete/:id')
+  .delete(postController.deleteJobPost);
 
 module.exports = router;
